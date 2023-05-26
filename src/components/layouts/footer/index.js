79d9b2d809a31ce5488767381373d318f19d2b8a
MinhTuan2502 <TuanNTMSE160480@fpt.edu.vn>
2023-05-26 16:38:05 +0700

    tuanpush fe

--- src/components/layouts/footer/index.js
+++ src/components/layouts/footer/index.js
@@ -24,22 +24,7 @@ function Footer() {
                             <li><i className={cx('fa fa-phone mr-2')}></i> + 123456789</li>
                             <li><i className={cx('fa fa-print mr-2')}></i> + 123456789</li>
                         </ul>
-                        <ul className={cx('list-unstyled')}>
-                            <li><i className={cx('fa fa-envelope mr-2')}></i> nghienmohinh@gmail.com</li>
-                            <li><i className={cx('fa fa-phone mr-2')}></i> + 123456789</li>
-                            <li><i className={cx('fa fa-print mr-2')}></i> + 123456789</li>
-                        </ul>
-
                     </div>
-                    <div className={cx('col-md-4 col-lg-4 col-xl-4')}>
-                        <h5>Giới thiệu</h5> 
-                         {/* <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">  */}
-                         <p className={cx('mb-0')}>
-                            Nếu bạn là một Wibu chính hiệu, thì còn chần chờ gì nữa mà không tậu ngay một em mô hình về để trưng bày.
-                        </p>
-                    </div>
-                    
-                    
                     <div className={cx('col-12 copyright mt-3')}>
                         <p class={cx('float-left')}>
                             <a href="#">Back to top</a>
--- src/pages/logins/LoginTeacher.js
+++ src/pages/logins/LoginTeacher.js
@@ -3,6 +3,12 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
+import classNames from "classnames/bind";
+import styles from "./login.module.scss";
+import Header from "~/components/layouts/header"; 
+
+const cx = classNames.bind(styles);
+
 function LoginTeacher() {
   const [username, setusername] = useState("");
   const [password, setPassword] = useState("");
@@ -29,30 +35,39 @@ function LoginTeacher() {
       });
   }
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setusername(e.target.value)}
-            name="username"
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            name="username"
-          />
-        </div>
-        <button type="submit">Login</button>
+    <>
+    <Header />
+    <div className={cx('login')}>
+      <form onSubmit={handleSubmit} className={cx('form')}>
+        <h2 className={cx('heading')}>Login</h2>
+          <div className={cx('form-group')}>
+            <label className={cx('form-label')}>Username:</label>
+            <input
+              className={cx('form-control')}
+              placeholder="VD: email@gmail.com"
+              type="text"
+              value={username}
+              onChange={(e) => setusername(e.target.value)}
+              name="username"
+            />
+            <span className={cx('form-message')}></span>
+          </div>
+          <div className={cx('form-group')}>
+            <label className={cx('form-label')}>Password:</label>
+            <input
+              className={cx('form-control')}
+              placeholder="Nhập mật khẩu"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="username"
+            />
+            <span className={cx('form-message')}></span>
+          </div>
+          <button type="submit" className={cx('form-submit')}>Login</button>
       </form>
     </div>
+    </>
   );
 }
 
