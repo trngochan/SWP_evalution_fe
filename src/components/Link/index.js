c1734eed98c3d41c46164ed403877dba4f963ce1
MinhTuan2502 <TuanNTMSE160480@fpt.edu.vn>
2023-05-26 15:37:55 +0700

    tuanpush

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,14 +1,17 @@
 import classNames from 'classnames/bind';
 import styles from './Loginbutton.module.scss'
 import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
 
 const cx = classNames.bind(styles); 
 
 function Loginbutton({children, to}) {
     return ( 
-        <Link  to= {to}> <Button variant="primary" size='lg'>{children}</Button> </Link>
-     );
+        <Link className={cx("btn")} to= {to}>
+            <button className={cx('btn-list')}>
+                {children}
+            </button>
+        </Link>
+    );
 }
 
 export default Loginbutton;
