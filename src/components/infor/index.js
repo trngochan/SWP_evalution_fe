2cc000818a22923948d036bf5c06b9dabf7b1ef4
trngochan <85632212+trngochan@users.noreply.github.com>
2023-05-26 16:12:05 +0700

    Han push infor

--- src/components/infor/index.js
+++ src/components/infor/index.js
@@ -1,13 +1,23 @@
 import classNames from "classnames/bind";
 import styles from "./infor.module.scss";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 function Infor({ name }) {
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies("token");
+
+  const handleLogout = () => {
+    removeCookie("token")
+    navigate('/');
+  }
+
   return (
     <div className={cx("wrapper")}>
         <button className={cx('btn')}><p className={cx("name")}>{name}</p></button>
-      <button className={cx('btn',"btn-primary", "btn-logout")}>Logout</button>
+      <button onClick={handleLogout} className={cx('btn',"btn-primary", "btn-logout")}>Logout</button>
     </div>
   );
 }
