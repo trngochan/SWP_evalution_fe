9fd4e15818e689965bf83e3bfb008828eefc9e64
trngochan <85632212+trngochan@users.noreply.github.com>
2023-05-29 16:45:22 +0700

    Han add add STD list

--- src/pages/create/AddStudentList.js
+++ src/pages/create/AddStudentList.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 import classNames from "classnames/bind";
 import styles from "../logins/login.module.scss";
@@ -9,124 +11,142 @@ import styles from "../logins/login.module.scss";
 const cx = classNames.bind(styles);
 
 function AddStudentList() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [code, setCode] = useState("");
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [role, setRole] = useState("");
-  // const navigate = useNavigate();
-
-  // const [cookies, setCookie, removeCookie] = useCookies("token");
-  function handleSubmit(event) {
-  //   event.preventDefault();
-
-  //   axios
-  //     .post("http://localhost:9000/loginteacher", {
-  //       username,
-  //       password,
-  //     })
-  //     .then((res) => res.data)
-  //     .then((data) => {
-  //       if (data.data.length > 0) {
-  //         setCookie("token", data.token, { path: "/" });
-  //       }
-  //       if (data.data.length > 0) navigate("/teacher");
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  }
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+      code: "",
+      name: "",
+      address: "",
+      birthday: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(5, "Your name must be at least 5 characters long")
+        .max(25, "Your name must be under 25 characters long")
+        .required("Your name is required"),
+      address: Yup.string().required("Your address is required"),
+      username: Yup.string()
+        .min(5, "Your username must be at least 5 characters long")
+        .required("Your username is required"),
+      birthday: Yup.date().required("Your birthday is required"),
+      password: Yup.string()
+        .min(5, "Your password must be at least 5 characters long")
+        .required("Your password is required"),
+      code: Yup.string().min(5, "Your code must be at least 5 characters long"),
+    }),
+    onSubmit: (values) => {
+      axios
+        .post("http://localhost:9000/student/add", values)
+        .then((res) => res.data)
+        .then((data) => {
+          // navigate("/admin");
+          console.log("thanh cong");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  });
   return (
     <>
-    <div className={cx('login')}>
-      <form onSubmit={handleSubmit} className={cx('form')}>
-        <h2 className={cx('heading')}>Add Student</h2>
-          <div className={cx('form-group')}>
-            <label className={cx('form-label')}>Code:</label>
+      <div className={cx("login")}>
+        <form onSubmit={formik.handleSubmit} className={cx("form")}>
+          <h2 className={cx("heading")}>Add Student</h2>
+          <div className={cx("form-group")}>
+            <label className={cx("form-label")}>Code:</label>
             <input
-              className={cx('form-control')}
-              placeholder="Enter Code"
+              className={cx("form-control")}
+              placeholder="Enter code"
               type="text"
-              value={code}
-              onChange={(e) => setCode(e.target.value)}
-              name="username"
+              name="code"
+              value={formik.values.code}
+              onChange={formik.handleChange}
             />
+            {formik.errors.code && formik.touched.code && (
+              <span className={cx("form-message")}>{formik.errors.code}</span>
+            )}
           </div>
-          <div className={cx('form-group')}>
-            <label className={cx('form-label')}>Name:</label>
+          <div className={cx("form-group")}>
+            <label className={cx("form-label")}>Name:</label>
             <input
-              className={cx('form-control')}
+              className={cx("form-control")}
               placeholder="Enter Name"
-              type="password"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              name="username"
+              type="text"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
             />
-            <span className={cx('form-message')}></span>
+            {formik.errors.name && formik.touched.name && (
+              <span className={cx("form-message")}>{formik.errors.name}</span>
+            )}
           </div>
-          <div className={cx('form-group')}>
-            <label className={cx('form-label')}>Birthday:</label>
+          <div className={cx("form-group")}>
+            <label className={cx("form-label")}>Birthday:</label>
             <input
-              className={cx('form-control')}
+              className={cx("form-control")}
               type="date"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              name="username"
+              name="birthday"
+              value={formik.values.birthday}
+              onChange={formik.handleChange}
             />
-            <span className={cx('form-message')}></span>
+            {formik.errors.date && formik.touched.date && (
+              <span className={cx("form-message")}>{formik.errors.date}</span>
+            )}
           </div>
-          <div className={cx('form-group')}>
-            <label className={cx('form-label')}>Address:</label>
+          <div className={cx("form-group")}>
+            <label className={cx("form-label")}>Address:</label>
             <input
-              className={cx('form-control')}
+              className={cx("form-control")}
               placeholder="Enter Address"
               type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              name="username"
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              name="address"
             />
-            <span className={cx('form-message')}></span>
+            {formik.errors.address && formik.touched.address && (
+              <span className={cx("form-message")}>
+                {formik.errors.address}
+              </span>
+            )}
           </div>
-          <div className={cx('form-group')}>
-            <label className={cx('form-label')}>Username:</label>
+          <div className={cx("form-group")}>
+            <label className={cx("form-label")}>Username:</label>
             <input
-              className={cx('form-control')}
+              className={cx("form-control")}
               placeholder="Enter Username"
-              type="password"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              value={formik.values.username}
+              onChange={formik.handleChange}
               name="username"
             />
-            <span className={cx('form-message')}></span>
+            {formik.errors.username && formik.touched.username && (
+              <span className={cx("form-message")}>
+                {formik.errors.username}
+              </span>
+            )}
           </div>
-          <div className={cx('form-group')}>
-            <label className={cx('form-label')}>Password:</label>
+          <div className={cx("form-group")}>
+            <label className={cx("form-label")}>Password:</label>
             <input
-              className={cx('form-control')}
+              className={cx("form-control")}
               placeholder="Enter password"
               type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              name="username"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              name="password"
             />
-            <span className={cx('form-message')}></span>
+            {formik.errors.password && formik.touched.password && (
+              <span className={cx("form-message")}>
+                {formik.errors.password}
+              </span>
+            )}
           </div>
-          <div className={cx('form-group')}>
-            <label className={cx('form-label')}>Role:</label>
-            <input
-              className={cx('form-control')}
-              placeholder="Enter Role"
-              type="text"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-              name="username"
-            />
-            <span className={cx('form-message')}></span>
-          </div>
-          <button type="submit" className={cx('form-submit')}>Add</button>
-      </form>
-    </div>
+          <button type="submit" className={cx("form-submit")}>
+            Add
+          </button>
+        </form>
+      </div>
     </>
   );
 }
