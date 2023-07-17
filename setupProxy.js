4aa112f24a75bb1b16e76e799e263e2fe9c29c95
trngochan <85632212+trngochan@users.noreply.github.com>
2023-07-17 18:00:51 +0700

    h

--- setupProxy.js
+++ setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware({
-      target: "https://fpt.onrender.com" || "http://localhost:9000",
+      target: "https://fpt.onrender.com:9000" || "http://localhost:9000",
       changeOrigin: true,
     })
   );
