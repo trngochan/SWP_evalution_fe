a5efa7b03b6b08dbdb1abad70eb84ca87362bbd5
trngochan <85632212+trngochan@users.noreply.github.com>
2023-07-17 16:06:32 +0700

    h

--- setupProxy.js
+++ setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware({
-      target: process.env.URL_BACKEND || "http://localhost:9000",
+      target: "https://fpt.onrender.com" || "http://localhost:9000",
       changeOrigin: true,
     })
   );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,30 +7,39 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Footer from "~/components/layouts/footer";
 import Button from "~/components/button";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
 
 function Home() {
+  console.log(process.env.URL_BACKEND);
   return (
     <div className={cx("container-main")}>
       <Header />
       <div className={cx("body")}>
-        <h2 className={cx("login")}>Login <FontAwesomeIcon icon={faRightToBracket} /></h2>
+        <h2 className={cx("login")}>
+          Login <FontAwesomeIcon icon={faRightToBracket} />
+        </h2>
         <Container>
           <Row className={cx("links")}>
             <Col>
-              <Button primary to={"/loginstudent"}>Login for Student</Button>
+              <Button primary to={"/loginstudent"}>
+                Login for Student
+              </Button>
             </Col>
             <Col>
-              <Button primary to={"/loginteacher"}>Login for Lecturer</Button>
+              <Button primary to={"/loginteacher"}>
+                Login for Lecturer
+              </Button>
             </Col>
             <Col>
-              <Button primary to={"/loginadmin"}>Login for Admin</Button>
+              <Button primary to={"/loginadmin"}>
+                Login for Admin
+              </Button>
             </Col>
           </Row>
-            <p className={cx("separate")}></p>
+          <p className={cx("separate")}></p>
         </Container>
       </div>
       <Footer />
